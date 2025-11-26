9b9d9aacb21f7a11d858995f5cb40251c950bab5
Diego Jiménez Vivaldi <diegojimenezvivaldi@gmail.com>
2025-11-26 19:35:59 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import subprocess
 
 print("Running supervised models...")
-subprocess.run(["python", "src/supervised/supervised_models.py"])
+subprocess.run(["python", "src/supervised/supervised_models_drive.py"])
 
 print("Running unsupervised models...")
-subprocess.run(["python", "src/unsupervised/unsupervised_models.py"])
+subprocess.run(["python", "src/unsupervised/unsupervised_models_drive.py"])
